561fb3394dd8080ae7896aaf57f7979477bae4fd
ssenoga <41122179+ssenoga@users.noreply.github.com>
2020-08-05 11:45:10 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>This is going to be amazon clone</h1>
+    </div>
+  );
+}
